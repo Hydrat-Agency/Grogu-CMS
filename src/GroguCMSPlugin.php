66a34b96279df39f519836666729673e042bea3f
tgeorgel <tgeorgel@users.noreply.github.com>
2024-02-29 08:32:21 +0000

    Fix styling

--- src/GroguCMSPlugin.php
+++ src/GroguCMSPlugin.php
@@ -2,8 +2,8 @@
 
 namespace Hydrat\GroguCMS;
 
-use Filament\Panel;
 use Filament\Contracts\Plugin;
+use Filament\Panel;
 use Hydrat\GroguCMS\Facades\GroguCMS as GroguCMSFacade;
 use Hydrat\GroguCMS\Filament\Resources\MenuResource;
 
