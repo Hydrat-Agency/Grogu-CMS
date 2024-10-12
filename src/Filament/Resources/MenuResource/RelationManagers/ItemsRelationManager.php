2a2811269c199fcef7574e0b11aa02537e20db92
tgeorgel <tgeorgel@users.noreply.github.com>
2024-10-12 14:44:09 +0000

    Fix styling

--- src/Filament/Resources/MenuResource/RelationManagers/ItemsRelationManager.php
+++ src/Filament/Resources/MenuResource/RelationManagers/ItemsRelationManager.php
@@ -4,17 +4,17 @@ namespace Hydrat\GroguCMS\Filament\Resources\MenuResource\RelationManagers;
 
 use Closure;
 use Filament\Forms;
-use Filament\Tables;
-use Filament\Forms\Get;
 use Filament\Forms\Form;
-use Filament\Tables\Table;
-use Illuminate\Support\Arr;
-use Filament\Tables\Grouping\Group;
-use Hydrat\GroguCMS\Models\MenuItem;
-use Hydrat\GroguCMS\Facades\GroguCMS;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Get;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Grouping\Group;
+use Filament\Tables\Table;
+use Hydrat\GroguCMS\Facades\GroguCMS;
+use Hydrat\GroguCMS\Models\MenuItem;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 class ItemsRelationManager extends RelationManager
 {
