8aecbf2e08a6dd89a4a3178f9e03e1ccaa28f52c
tgeorgel <tgeorgel@users.noreply.github.com>
2024-10-13 12:33:51 +0000

    Fix styling

--- src/Models/FormField.php
+++ src/Models/FormField.php
@@ -2,12 +2,12 @@
 
 namespace Hydrat\GroguCMS\Models;
 
-use Spatie\EloquentSortable\Sortable;
-use Illuminate\Database\Eloquent\Model;
 use Hydrat\GroguCMS\Enums\FormFieldType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations;
+use Spatie\EloquentSortable\Sortable;
 use Spatie\EloquentSortable\SortableTrait;
 
 class FormField extends Model implements Sortable
