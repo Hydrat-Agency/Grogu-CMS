1a4dc564c5914c2ce4bb06fa5da37899b83467f9
Thomas <themagnetiiktbk@gmail.com>
2025-01-12 14:13:40 +0100

    :bug: Fix EditRecord class name

--- src/Filament/Resources/CmsResource/Pages/EditRecord.php
+++ src/Filament/Resources/CmsResource/Pages/EditRecord.php
@@ -3,13 +3,13 @@
 namespace Hydrat\GroguCMS\Filament\Resources\CmsResource\Pages;
 
 use Filament\Actions;
-use Filament\Resources\Pages\EditRecord;
+use Filament\Resources\Pages\EditRecord as BaseEditRecord;
 use Hydrat\GroguCMS\Contracts\BlueprintContract;
 use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\HtmlString;
 
-abstract class EditPage extends EditRecord
+abstract class EditRecord extends BaseEditRecord
 {
     public static function getBlueprint($filament = null): BlueprintContract
     {
